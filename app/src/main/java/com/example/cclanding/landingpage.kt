2633c22a10b6c17e97145c46224c0f5afc316c39
RookieCookie56 <egaming.ea@gmail.com>
2023-05-17 16:53:05 +0800

    17/5

---
+++ app/src/main/java/com/example/cclanding/landingpage.kt
@@ -0,0 +1,12 @@
+package com.example.cclanding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class landingpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
